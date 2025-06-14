cd78125cb3e552ca2fa8ab07fe5403aa3e33334a
Caique Azevedo <caiqueazevedo2005@gmail.com>
2025-06-14 02:09:18 -0300

    refactor(footer): melhora acessibilidade e semântica

--- luxos-brecho/src/components/Footer/index.jsx
+++ luxos-brecho/src/components/Footer/index.jsx
@@ -8,23 +8,30 @@ import paypalLogo from '../../assets/paypal 1.png';
 import gpayLogo from '../../assets/pagamento-do-google 1.png';
 
 const Footer = () => (
-  <footer className="footer">
+  <footer className="footer" role="contentinfo">
     <div className="footer-content">
       <div className="footer-left">
         <img src={instagramIcon} alt="Instagram" className="footer-instagram" />
         <a href="https://www.instagram.com/luxus.brecho_/" target="_blank" rel="noopener noreferrer" className="footer-brand">luxus.brecho_</a>
-        <nav className="footer-nav">
-          <Link to="/sobre">Sobre Nós</Link>
-          <Link to="/suporte">Suporte</Link>
-          <Link to="/contato">Contato</Link>
+        <nav className="footer-nav" aria-label="Footer Navigation">
+          <ul>
+            <li><Link to="/sobre">Sobre Nós</Link></li>
+            <li><Link to="/suporte">Suporte</Link></li>
+            <li><Link to="/contato">Contato</Link></li>
+          </ul>
         </nav>
       </div>
-      <div className="footer-right">
-        <img src={visaLogo} alt="Visa" />
-        <img src={mastercardLogo} alt="Mastercard" />
-        <img src={paypalLogo} alt="PayPal" />
-        <img src={gpayLogo} alt="G Pay" />
+      <div className="footer-right" aria-label="Métodos de Pagamento">
+        <ul>
+          <li><img src={visaLogo} alt="Visa" /></li>
+          <li><img src={mastercardLogo} alt="Mastercard" /></li>
+          <li><img src={paypalLogo} alt="PayPal" /></li>
+          <li><img src={gpayLogo} alt="G Pay" /></li>
+        </ul>
       </div>
+    </div>
+    <div className="footer-copyright">
+      <p>© {new Date().getFullYear()} Luxus Brechó. Todos os direitos reservados.</p>
     </div>
   </footer>
 );
