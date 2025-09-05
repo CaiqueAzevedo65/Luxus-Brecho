04ed230f544f9187355c59d416af7f88b5468230
Caique Azevedo <caiqueazevedo2005@gmail.com>
2025-09-04 21:15:50 -0300

    refactor(mobile): correção da arquitetura e dependências

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,17 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./App.{js,jsx}", "./screens/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
-    theme: {
-      extend: {
-        colors: {
-          primary: '#FF6F61',
-          secondary: '#333333',
-        },
-        fontFamily: {
-          sans: ['Inter', 'sans-serif'],
-        },
+  // NOTE: Update this to include the paths to all of your component files.
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6F61',
+        secondary: '#333333',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
       },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
